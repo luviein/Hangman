69d0a145518d679d6da0f79e170e6d868498da11
Yenleng <yenlengggggg@gmail.com>
2023-05-09 16:14:34 +0800

    add get random item from array

--- Alphabets.java
+++ Alphabets.java
@@ -1,3 +1,19 @@
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.sound.sampled.SourceDataLine;
+
 public class Alphabets {
+    static String[] words = {"apple", "banana", "cherry", "orange", "grape", "lemon", "lime", "peach", "pear", "plum"};
     
+    public static String getRandomWords(){
+        Random random = new Random();
+        int index = random.nextInt(words.length);
+        return words[index];
+    }
+    
+
+
+
 }
+
---
+++ Hangman.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Hangman{
+
+    public static void main(String[] args) {
+        System.out.println(Alphabets.getRandomWords());
+    }
+
+
+
+}
