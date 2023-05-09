6dff551214a476ff3cfe90ae0f48269a2604712d
Yenleng <yenlengggggg@gmail.com>
2023-05-09 23:20:26 +0800

    add loops to check answer

--- Alphabets.java
+++ Alphabets.java
@@ -12,7 +12,7 @@ public class Alphabets {
         return words[index];
     }
     
-
+    
 
 
 }
--- Hangman.java
+++ Hangman.java
@@ -1,11 +1,73 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
-public class Hangman{
+import javax.sound.sampled.SourceDataLine;
 
+public class Hangman {
+
+    
     public static void main(String[] args) {
-        System.out.println(Alphabets.getRandomWords());
+        String[] word = Alphabets.getRandomWords().split("");
+        System.out.println("Answer: " + Arrays.toString(word));
+
+        
+        int guess = 5;
+
+        System.out.println("Welcome to the Hangman Game~");
+        System.out.println("You have 5 guesses. The game begins now...");
+
+        List<String> guessArray = new ArrayList<>();
+        int i = 0;
+        while (i < word.length) {
+            guessArray.add("_ ");
+            i++;
+        }
+        
+
+
+        while (guess <= 5 && guess > 0) {
+
+            // System.out.println(guessArray);
+
+            Scanner scanner = new Scanner(System.in);
+
+            // loop through first character and see if it matches any of the char in the
+            // random word
+            // List<String> userAns = new ArrayList<>();
+
+            for (int x = 0; x < word.length; x++) {
+
+                System.out.println("Your guess: ");
+                String userInput = scanner.nextLine().toLowerCase();
+                if (userInput.equals(word[x])) {
+                    System.out.println("Correct");
+                    guessArray.set(x, userInput);
+                    System.out.println(guessArray);
+
+                } else {
+                    System.out.println("Wrong");
+                    guess -= 1;
+                    System.out.println("Lives left: " + guess);
+                }
+
+            }
+            if (guess == 0){
+                System.out.println("You lost the game. Type 'y' to continue, 'n' to quit");
+                Scanner scanner2 = new Scanner(System.in);
+                String yesNo = scanner2.nextLine().toLowerCase();
+                if(yesNo == "n"){
+                   System.exit(0);
+                }else if(yesNo == "y"){
+                    guess = 5;
+                    continue;
+                }
+
+            } 
+
+        }
+        
     }
-
-
 
 }
