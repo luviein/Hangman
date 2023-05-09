fbb960caf980f8e59051d844fbcc37c25dbd1da8
Yenleng <yenlengggggg@gmail.com>
2023-05-10 03:14:27 +0800

    add ability to restart

--- Game.java
+++ Game.java
@@ -3,36 +3,38 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.sound.sampled.SourceDataLine;
+public class Game {
 
-public class Hangman {
+    // public static void newGuessArray(String[] word){
+    // List<String> guessArray = new ArrayList<>();
+    // int i = 0;
+    // while (i < word.length) {
+    // guessArray.add("_ ");
+    // i++;
+    // }
+    // System.out.println(guessArray);
+    // }
 
-    public static void main(String[] args) {
+    public static void gameStart() {
         String[] word = Alphabets.getRandomWords().split("");
         System.out.println("Answer: " + Arrays.toString(word));
 
-        int guess = 5;
-
-        
         List<String> guessArray = new ArrayList<>();
         int i = 0;
         while (i < word.length) {
             guessArray.add("_ ");
             i++;
         }
+        System.out.println("Welcome to the Hangman Game~");
+        System.out.println("You have 5 guesses. The game begins now...");
 
-        while (guess <= 5 && guess > 0) {
-            System.out.println("Welcome to the Hangman Game~");
-            System.out.println("You have 5 guesses. The game begins now...");
-        
-
-            // System.out.println(guessArray);
+        int guess = 5;
+        while (guess <= 5) {
 
             Scanner scanner = new Scanner(System.in);
 
             // loop through first character and see if it matches any of the char in the
             // random word
-            // List<String> userAns = new ArrayList<>();
 
             for (int x = 0; x < word.length; x++) {
 
@@ -40,28 +42,33 @@ public class Hangman {
                     Scanner scanner2 = new Scanner(System.in);
                     System.out.println("Y or N");
                     String yesNo = scanner2.nextLine().trim().toLowerCase();
-                    if(yesNo.equals("y")){
+                    if (yesNo.equals("y")) {
+                        System.out.println("Welcome to the Hangman Game~");
+                        System.out.println("You have 5 guesses. The game begins now...");
                         guess = 5;
-                        //String[] word = Alphabets.getRandomWords().split("");
-                        System.out.println("Answer: " + Arrays.toString(word));
-                        System.out.println(guessArray);
+                        guessArray.clear();
+                        String[] words = Alphabets.getRandomWords().split("");
+                        System.out.println("Answer: " + Arrays.toString(words));
+                        int j = 0;
+                        while (j < word.length) {
+                            guessArray.add("_ ");
+                            j++;
+                        }
 
-   
-                    }else if(yesNo.equals("n")){
+                        continue;
+                    } else if (yesNo.equals("n")) {
                         System.out.println("Goodbye~");
                         System.exit(0);
                     }
-                
-                }
-                
 
+                }
+                System.out.println(guessArray);
                 System.out.println("Your guess: ");
                 String userInput = scanner.nextLine().toLowerCase().trim();
-                
+
                 if (userInput.equals(word[x])) {
                     System.out.println("Correct");
                     guessArray.set(x, userInput);
-                    System.out.println(guessArray);
 
                 } else {
                     System.out.println("Wrong");
@@ -70,14 +77,11 @@ public class Hangman {
                 }
 
             }
-            if(guessArray.equals(Arrays.asList(word))){
+            if (guessArray.equals(Arrays.asList(word))) {
                 System.out.println("You win!");
                 System.exit(0);
             }
 
-            
-
-        }   
-
+        }
     }
 }
---
+++ Hangmancopy.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Hangmancopy {
+
+    public static void main(String[] args) {
+        Game.gameStart();
+
+
+    }
+}
