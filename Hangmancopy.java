5f91e744952202724338890a9182b1c6355a88d2
Yenleng <yenlengggggg@gmail.com>
2023-05-10 03:16:19 +0800

    code clean up at Hangmancopy.java

--- Hangmancopy.java
+++ Hangmancopy.java
@@ -1,15 +1,8 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-import javax.sound.sampled.SourceDataLine;
 
 public class Hangmancopy {
 
     public static void main(String[] args) {
         Game.gameStart();
-
 
     }
 }
